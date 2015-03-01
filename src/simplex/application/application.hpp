3086ae3f6f0fde3d77acd5a18fd678f6cc3442bb
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2015-03-01 11:46:40 +0100

    added comments

--- src/simplex/application/application.hpp
+++ src/simplex/application/application.hpp
@@ -12,14 +12,14 @@ namespace simplex {
 /// <summary>	Minimal class representing an application. </summary>
 class application {
    public:
-       ////////////////////////////////////////////////////////////////////////////////////////////////////
-       /// <summary>	Initializes the application. </summary>
-       ///
-       /// <param name="program_args">	The program arguments. </param>
-       /// <param name="assets">	  	The asset factory. </param>
-       ////////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>	Initializes the application. </summary>
+    ///
+    /// <param name="program_args">	The program arguments. </param>
+    /// <param name="assets">	  	The asset factory. </param>
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
     application(std::unique_ptr<program_arguments> program_args, asset_loader::constructor asset_loader_constructor)
-		: program_args(std::move(program_args)), assets(std::move(asset_loader_constructor(get_program_args()))){};
+        : program_args(std::move(program_args)), assets(std::move(asset_loader_constructor(get_program_args()))){};
 
     /// <summary>	Destructor. </summary>
     virtual ~application(){};
@@ -27,12 +27,23 @@ class application {
     /// <summary>	Runs the main loop of the application. </summary>
     virtual void run() = 0;
 
-	program_arguments const& get_program_args() const { return *program_args; }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>	Returns the program arguments. </summary>
+    ///
+    /// <returns>	The program arguments. </returns>
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    program_arguments const& get_program_args() const { return *program_args; }
 
-	virtual bool on_resize(glm::ivec2 new_size) = 0;
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>	Called when the window is resizd. </summary>
+    ///
+    /// <param name="new_size">	Size of the new window. </param>
+    ///
+    /// <returns>	true if event was handled, false if not. </returns>
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    virtual bool on_resize(glm::ivec2 new_size) = 0;
 
-
-protected:
+   protected:
     std::unique_ptr<program_arguments> program_args;
     std::unique_ptr<asset_loader> assets;
 };
